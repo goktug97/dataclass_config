bcef7f4b35e798bc637221914ed2968335497fb6
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2021-09-28 23:21:48 +0300

    update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,9 @@
-from .dataclass_config import Config, Argument, Choice
+from .dataclass_config import Config, Argument
 from .dataclass_config import check_required, RequiredError, Required
 
 
 __all__ = ['Config',
         'Argument',
-        'Choice',
         'check_required',
         'Required',
         'RequiredError']
