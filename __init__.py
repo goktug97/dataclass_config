36059b46949e065ba231838021d566f3309dbb21
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2021-09-28 20:50:44 +0300

    add init

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .dataclass_config import Config, Argument, Choice
+from .dataclass_config import check_required, RequiredError
+
+
+__all__ = ['Config',
+        'Argument',
+        'Choice',
+        'check_required',
+        'Required',
+        'RequiredError']
+
